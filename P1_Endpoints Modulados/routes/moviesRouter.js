a2b9f950830d08caafe4da55ecfd767c513c98e7
SHW378 <cesarbrmn@hotmail.com>
2025-09-24 16:22:29 -0600

    endpoint patch

--- P1_Endpoints Modulados/routes/moviesRouter.js
+++ P1_Endpoints Modulados/routes/moviesRouter.js
@@ -3,18 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 let movies = [
-  {id: 1, title: 'Como entrenar a tu Dragon', year: 2010, Category: 'Animada'},
-  {id: 2, title: 'Una pelicula de nuevos', year:2000, Category: 'Animada'},
-  {id: 3, title: 'Interestellar', year:2014, Category: 'Scr-Fi'},
-  {id: 4, title: 'Lalaland', year:2010, Category: 'Musical'},
-  {id: 5, title: 'Cuestion de tiempo', year:2014, Category: 'Romance'},
-  {id: 6, title: 'GoodFella', year:1996, Category: 'Suspenso'},
-  {id: 7, title: 'Mision Rescate', year:2015, Category: 'Sci-Fi'},
-  {id: 8, title: 'Clouds', year:2018, Category: 'Live Action'},
-  {id: 9, title: 'Batman Return', year:2010, Category: 'Action'},
-  {id: 10, title: 'Me Before you', year:2015, Category: 'Romance'},
-  {id: 11, title: 'Perdida', year:2012, Category: 'Drama'},
-  {id: 12, title: '¿Y donde estan las rubias', year:2004, Category: 'Comedia'},
+  { id: 1, title: 'Como entrenar a tu Dragon', year: 2010, Category: 'Animada' },
+  { id: 2, title: 'Una pelicula de nuevos', year: 2000, Category: 'Animada' },
+  { id: 3, title: 'Interestellar', year: 2014, Category: 'Scr-Fi' },
+  { id: 4, title: 'Lalaland', year: 2010, Category: 'Musical' },
+  { id: 5, title: 'Cuestion de tiempo', year: 2014, Category: 'Romance' },
+  { id: 6, title: 'GoodFella', year: 1996, Category: 'Suspenso' },
+  { id: 7, title: 'Mision Rescate', year: 2015, Category: 'Sci-Fi' },
+  { id: 8, title: 'Clouds', year: 2018, Category: 'Live Action' },
+  { id: 9, title: 'Batman Return', year: 2010, Category: 'Action' },
+  { id: 10, title: 'Me Before you', year: 2015, Category: 'Romance' },
+  { id: 11, title: 'Perdida', year: 2012, Category: 'Drama' },
+  { id: 12, title: '¿Y donde estan las rubias', year: 2004, Category: 'Comedia' },
 ]
 
 //Obtener todas las peliculas
@@ -29,7 +29,7 @@ router.get('/:id', (req, res) => {
   if (movie) {
     res.json(movie);
   } else {
-    res.status(404).json({message: 'Movie not found'})
+    res.status(404).json({ message: 'Movie not found' })
   }
 
 })
@@ -47,3 +47,23 @@ router.post('/', (req, res) => {
     data: newMovie
   });
 })
+
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const movieID = parseInt(id);
+  const { title, year, category } = req.body;
+  const movie = movies.find(m => m.id === movieID);
+  if (movie) {
+    if (title) movie.title = title;
+    if (year) movie.year = year;
+    if (category) movie.category = category;
+    res.json({
+      message: 'updated',
+      data: movie
+    })
+  } else {
+    res.status(404).json({ message: 'Movie not found' })
+  }
+
+});
