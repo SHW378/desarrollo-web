843cc0ac17be112bff61e77893586d4c2833470d
Jose <jvc81668@lasallebajio.edu.mx>
2025-11-18 03:47:56 -0600

    debug rutas. Api, V1

--- P1_Endpoints Modulados - copia/routes/rutas.js
+++ P1_Endpoints Modulados - copia/routes/rutas.js
@@ -6,33 +6,15 @@ const categoriesRouter = require('./CategoriesRouter');
 const brandsRouter = require('./BrandsRouter');
 
 function routerApi(app) {
-
-
   const router = express.Router();
-<<<<<<< HEAD
   // Prefijo para todas las rutas
   app.use('/', router);
+
   // Asigna las rutas
   router.use('/products', productsRouter);
   router.use('/users', userRouter);
   router.use('/categories', categoriesRouter);
   router.use('/brands', brandsRouter);
-=======
-  //Instancias de los servicios (sin dependencias primero)
-  const usersService = new UsersService();
-  const categoriesService = new CategoriesService();
-  const brandsService = new BrandsService();
-  //Se crea el servicio de productos con las dependencias
-  const productsService = new ProductsService(brandsService, categoriesService);
-  //Instancia de los routers
-  router.use('/products', productsRouter(productsService));
-  router.use('/users', userRouter(usersService));
-  router.use('/categories', categoriesRouter(categoriesService, productsService));
-  router.use('/brands', brandsRouter(brandsService, productsService));
-
-      app.use('/', router);
-
->>>>>>> 47b4dc4 ( Debugueo ip de swagger e index y agrego funcion en rutas)
 }
 
 module.exports = routerApi;
